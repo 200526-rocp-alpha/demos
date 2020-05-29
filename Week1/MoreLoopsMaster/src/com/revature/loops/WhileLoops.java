ba2c0c7d9c6fa0ee483f8bd3780a2a4474cbd9f7
KodaZys <msophiagavrila@gmail.com>
2020-05-29 13:25:41 -0400

    removed tester class

---
+++ Week1/MoreLoopsMaster/src/com/revature/loops/WhileLoops.java
@@ -0,0 +1,53 @@
+package com.revature.loops;
+
+public class WhileLoops {
+
+	public static void main(String[] args) {
+
+//		// While loops:
+//		// while *something* is true, do something.
+//		
+////		while (true) {
+////			//this is the code to execute
+////			System.out.println("Looping!");
+////		}
+//		// a while(true) loop will loop for infinity 
+//		
+//		int count = 0;
+//		
+//		while (count < 10) {
+//			System.out.println("looping " + count);
+//			count++;		//count = count + 1;
+//			
+//			if (count == 7) {
+//				break;     				// break will force us to exit this loop.
+//			}
+//		}
+//		
+//		boolean flag = false;
+//		
+//		do {
+//			System.out.println("printing!!!!");
+//		} while (flag == false);
+//		
+		
+		int value = 0;
+//		
+//		while (value<10) {
+//			System.out.println("printing the while loop");
+//		}
+		
+	
+		do {
+			// do some code
+			System.out.println("Value is less than 10 - The do-while loop is looping!");
+			value++;
+		} while (value < 10); //while this condition is true
+		
+
+		
+		
+		
+	}
+
+}
