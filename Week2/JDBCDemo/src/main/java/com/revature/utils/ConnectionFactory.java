23a9aab07c4162b702a5d3a8dd2d7cf9ae483cb6
KodaZys <msophiagavrila@gmail.com>
2020-06-09 22:26:45 -0400

    FIXED CONNECTION STRING TO RDS IN CONNECTION FACTORY

--- Week2/JDBCDemo/src/main/java/com/revature/utils/ConnectionFactory.java
+++ Week2/JDBCDemo/src/main/java/com/revature/utils/ConnectionFactory.java
@@ -7,11 +7,13 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 
 	public static Connection getConnection() {
-//		
-//		String url = "jdbc:oracle:thin:@localhost:1521:firstDB";
+		
+//		this is for connecting to AWS RDS!
+//		String url = "jdbc:oracle:thin:@database-2.c0rzi76acgyn.us-east-1.rds.amazonaws.com:1521:FIRSTDB";
 //		String username = "admin";
 //		String password = "12345678";
 		
+		// this is for connecting to local DB (on your machine)
 		String url = "jdbc:oracle:thin:@localhost:1521:orcl";
 		String username = "sophia";
 		String password = "123";
